df7cb2a171889bda5fc11bc381fbd222135d77cb
Kierra M <k.merriman32@hotmail.com>
2019-11-29 16:40:56 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 ENV['SINATRA_ENV'] ||= "development"
-
+#above is telling sinatra to use development env for shotgun and testing suite when we run migrations we run this top line too so all connects  rake db:migrate SINATTRA_ENV_development
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
 require './app'
